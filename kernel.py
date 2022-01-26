fd4f482dc43b3c9e2f0bd1043bd825c7ae2fd19e
sorawitj@gmail.com <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>
2022-01-26 22:57:55 +0000

    Clean up the repo

--- kernel.py
+++ kernel.py
@@ -11,40 +11,18 @@ class Kernels(torch.nn.Module):
         self.z = kernel_z
 
 
-class PTKGauss(torch.nn.Module):
-    """
-    Pytorch implementation of the isotropic Gaussian kernel.
-    Parameterization is the same as in the density of the standard normal
-    distribution. sigma2 is analogous to the variance.
-    """
+class RBFKernel(torch.nn.Module):
 
     def __init__(self, sigma):
-        """
-        sigma2: a number representing the squared bandwidth
-        """
         super().__init__()
-        assert sigma > 0, 'sigma2 must be > 0. Was %s' % str(sigma)
-        # need to be a tensor to make it tunable with a torch optimizer
+        assert sigma > 0, 'sigma must be > 0. Current %s' % str(sigma)
         self.sigma = sigma
-        # _tunable_params = self.sigma2
 
     def set_kernel_param(self, sigma=None):
         self.sigma = to_torch(sigma)
 
     def forward(self, X):
-        """
-        Evaluate the Gaussian kernel on the two 2d Torch Tensors
-        Parameters
-        ----------
-        X : n1 x d Torch Tensor
-        Y : n2 x d Torch Tensor
-        Return
-        ------
-        K : a n1 x n2 Gram matrix.
-        """
-        # sigma = self.sigma
         sumX2 = (X ** 2).sum(1, keepdim=True)
-        # sumy2 = torch.sum(X ** 2, dim=1).view(1, -1)
         D2 = sumX2 - 2.0 * X.matmul(X.t()) + sumX2.t()
         D2 = D2.clamp(min=0)
 
@@ -53,13 +31,52 @@ class PTKGauss(torch.nn.Module):
 
 
 class CategoryKernel(torch.nn.Module):
-    def __init__(self):
+    def __init__(self, one_hot=True):
         super().__init__()
+        self.one_hot = one_hot
 
     def forward(self, Z):
-        # Z must be category
-        Z = Z.view(-1)
-        Z_unique, Z = torch.unique(Z, return_inverse=True)
-        Z = F.one_hot(Z, num_classes=Z_unique.size(0))
-        ret = (Z @ Z.T).to(torch.float)
+        if self.one_hot:
+            # Z must be category
+            Z = Z.view(-1)
+            Z_unique, Z = torch.unique(Z, return_inverse=True)
+            Z = F.one_hot(Z, num_classes=Z_unique.size(0))
+            ret = (Z @ Z.T).to(torch.float)
+        else:
+            ret = torch.nn.functional.cosine_similarity(Z[:,:,None], Z.t()[None,:,:])
+            # ret = (ret == 1).to(torch.float)
+        # ret = torch.nn.functional.cosine_similarity(Z[:,:,None], Z.t()[None,:,:])
         return ret
+
+class ProductKernel3(torch.nn.Module):
+
+    def __init__(self, k1, k2, k3, d1, d2, d3):
+        super().__init__()
+        self.k1 = k1
+        self.k2 = k2
+        self.k3 = k3
+        self.d1 = d1
+        self.d2 = d2
+        self.d3 = d3
+
+    def forward(self, X):
+  
+        K1 = self.k1(X[:, self.d1])
+        K2 = self.k2(X[:, self.d2])
+        K3 = self.k3(X[:, self.d3])
+        return K1 * K2 * K3
+    
+class ProductKernel2(torch.nn.Module):
+
+    def __init__(self, k1, k2, d1, d2):
+        super().__init__()
+        self.k1 = k1
+        self.k2 = k2
+        self.d1 = d1
+        self.d2 = d2
+
+    def forward(self, X):
+  
+        K1 = self.k1(X[:, self.d1])
+        K2 = self.k2(X[:, self.d2])
+        return K1 * K2
